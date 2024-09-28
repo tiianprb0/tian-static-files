37b3f6ae1204013cb09f047511b270990ceded1e
tiianprb0 <136343253+tiianprb0@users.noreply.github.com>
2024-09-28 14:49:33 +0700

    Update header scroll.js

--- header scroll.js
+++ header scroll.js
@@ -1,24 +1,25 @@
+// scroll-header.js
 
 var lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-window.addEventListener("scroll", function() {
-    var scrollHeaderNew = document.getElementById("scroll-header-new");
+var header = document.getElementById('scroll-header-new');
+
+window.addEventListener('scroll', function() {
     var st = window.pageYOffset || document.documentElement.scrollTop;
     var totalScrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
-    var hideThreshold = totalScrollableHeight * 0.1; // 10% before the top
+    var hideThreshold = totalScrollableHeight * 0.1; // 10% of total scrollable height
 
     if (st > lastScrollTop) {
         // Scrolling down
-        scrollHeaderNew.classList.remove("show-scroll-header-new");
-    } else {
+        header.classList.remove('show-scroll-header-new');
+    } else if (st < lastScrollTop) {
         // Scrolling up
         if (st <= hideThreshold) {
-            // Within 10% from the top; hide the header
-            scrollHeaderNew.classList.remove("show-scroll-header-new");
+            // Within 10% of the top; hide the header
+            header.classList.remove('show-scroll-header-new');
         } else {
             // Not near the top; show the header
-            scrollHeaderNew.classList.add("show-scroll-header-new");
+            header.classList.add('show-scroll-header-new');
         }
     }
     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
-]]>
+});
